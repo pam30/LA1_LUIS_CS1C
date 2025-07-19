eec8c26d8277a446c4524224fd0347239841635a
pam30 <202375371+pam30@users.noreply.github.com>
2025-07-19 18:57:16 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,26 @@
+// Task 6: Strings
+
+// 1. Favorite actor's first name
+let favActorFirstName = "Phil";
+
+// 2. Favorite actor's last name
+let favActorLastName = "Wickham";
+
+// 3. Concatenate first and last name
+let fullName = favActorFirstName + " " + favActorLastName;
+
+// 4. Uppercase version of the full name
+let uppercase = fullName.toUpperCase();
+
+// 5. Create message with the actor's name in uppercase
+let message = `My favorite actor is ${uppercase}`;
+
+// 6. Append message with additional information
+message += " and his best show is Silicon Valley.";
+
+// 7. Log all variables
+console.log("First Name:", favActorFirstName);
+console.log("Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Full Name:", uppercase);
+console.log("Message:", message);
