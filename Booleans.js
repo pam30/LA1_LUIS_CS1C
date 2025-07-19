2600321ff3479e4a2107472caa22fddd2ec3f70c
pam30 <202375371+pam30@users.noreply.github.com>
2025-07-19 18:34:56 +0800

    Create Booleans.js

---
+++ Booleans.js
@@ -0,0 +1,19 @@
+// Task 4: Booleans
+
+// 1. Create a variable named isJsProgrammingLanguage and store true
+let isJsProgrammingLanguage = true;
+
+// 2. Create a variable named isJsHard and store false
+let isJsHard = false;
+
+// 3. Create a variable named favNumb and store your favorite number
+let favNumb = 7; // You can change 7 to your favorite number
+
+// 4. Add favNumb with the value of undefined
+let result = favNumb + undefined;
+
+// 5. Log all variables to the console
+console.log("Is JavaScript a Programming Language? ", isJsProgrammingLanguage);
+console.log("Is JavaScript Hard? ", isJsHard);
+console.log("My Favorite Number: ", favNumb);
+console.log("Favorite Number + undefined = ", result);
